8f9783ba6889b153b3adeec4116a32ed685a4c90
cleberson.chagas <cleberson.chagas@syngenta.com>
2021-08-28 16:53:38 -0300

    adjust package names

--- src/main/java/br/com/hytech/rhsouthsystem/model/Vote.java
+++ src/main/java/br/com/hytech/rhsouthsystem/model/Vote.java
@@ -1,6 +1,7 @@
-package br.com.hytech.rhsouthsystem.entity;
+package br.com.hytech.rhsouthsystem.model;
 
 import br.com.hytech.rhsouthsystem.enums.VoteChoice;
+import br.com.hytech.rhsouthsystem.model.entity.AbstractEntity;
 import lombok.Data;
 import lombok.EqualsAndHashCode;
 import lombok.NoArgsConstructor;
@@ -8,17 +9,20 @@ import lombok.NoArgsConstructor;
 import javax.persistence.*;
 
 @Data
-@Table
+@Table(uniqueConstraints = {
+            @UniqueConstraint(columnNames = {"cd_associate", "cd_session"})
+        })
 @Entity
 @NoArgsConstructor
 @EqualsAndHashCode(callSuper = true)
-public class Vote extends AbstractEntity{
+public class Vote extends AbstractEntity {
 
     @ManyToOne
     @JoinColumn(name = "cd_associate")
     private Associate associate;
 
     @ManyToOne
+    @JoinColumn(name = "cd_session")
     private Session session;
 
     @Enumerated(EnumType.STRING)
